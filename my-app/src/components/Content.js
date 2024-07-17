71f541a3ea7ec26b3cfb4b6a96071597de9f69ce
markohautala <markohautala@live.se>
2024-07-17 08:33:11 +0200

    Fix search-post issue

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -3,7 +3,7 @@ import css from "./css/Content.module.css";
 import PostItem from "./PostItem";
 import savedPosts from "../data/posts.json"; // Ensure the path is correct
 import Loader from "./Loader";
-import SearchBar from "./SearchBar"; // Correct import
+import SearchPost from "./SearchPost";
 
 console.log(savedPosts); // Debug: Check if JSON data is loaded
 
@@ -27,13 +27,10 @@ export class Content extends Component {
   render() {
     return (
       <div className={css.Content} style={{ margin: '20px' }}>
-        <div className={css.TitleBar}>
-          <h1>My Photos</h1>
-          <SearchBar savedPosts={savedPosts.savedPosts} /> {/* Pass savedPosts */}
-        </div>
         <div className={css.SearchResults}>
+        <h1>My Photos</h1>
           {this.state.isLoaded ? (
-            <PostItem savedPosts={savedPosts.savedPosts} />
+            <SearchPost savedPosts={savedPosts.savedPosts} />
           ) : (
             <Loader />
           )}
